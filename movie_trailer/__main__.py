e49e29896b05267e22dd25163f77849f1d3fb054
chakradhar-s <venkatachakradhar@gmail.com>
2017-11-01 04:16:41 +0530

    final changes

--- movie_trailer/__main__.py
+++ movie_trailer/__main__.py
@@ -3,8 +3,9 @@ from fresh_tomatoes import open_movies_page
 from entertainment_center import Entertainment
 
 
-# I have taken reference from https://chriswarrick.com/listings/entry_points_project/my_project/__main__.py.html for the semantic 
-# of code
+# I have taken reference from 
+#https://chriswarrick.com/listings/entry_points_project/my_project/__main__.py.html
+#for maintaing the same semantic of code
 def main(args=None):
     """The movie trailer website's main routine and code executes from here"""
     if args is None:
@@ -12,7 +13,8 @@ def main(args=None):
 
     # Project script execution statements starts from below.
     entertainment = Entertainment()
-    # calls open_movies_page with a movie list as input from entertainment center, in fresh_tomatoes and opens a html file in browser
+    # calls open_movies_page with a movie list as input from entertainment 
+    # center, in fresh_tomatoes and opens a html file in browser
     open_movies_page(entertainment.get_movies_list())    
 
 if __name__ == "__main__":
